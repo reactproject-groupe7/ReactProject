3fa254806056b2585a155b643ea87815547cb092
najlabenchelly <chelly.n103@gmail.com>
2019-07-06 21:27:57 +0200

    get  profil User

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,35 @@
 const express = require('express');
 const router = express.Router();
-// @route GET api/profile
-// @desc Test route 
-// @access Public
+const auth = require('../../middleware/auth');
 
-router.get("/", (req, res) => res.send('Profile route'));
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+// @route  GET api/profile/me
+// @desc  GET current users profile
+// @access Private
+
+router.get('/me', auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({
+            user: req.user.id
+        }).populate(
+            'user',
+            ['name', 'avatar']
+        );
+
+        if (!profile) {
+            return res.status(400).json({
+                msg: 'Profil inexistant'
+            });
+        }
+
+        res.json(profile);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
 
 
 module.exports = router;
