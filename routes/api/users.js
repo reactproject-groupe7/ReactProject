b668a9544dcaebea9039a0636c8c546fe415c02b
najlabenchelly <chelly.n103@gmail.com>
2019-07-06 15:27:37 +0200

    utilisateeur inscription

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 const {
     check,
     validationResult
 } = require('express-validator/check');
+const User = require('../../models/User');
 // @route POST api/users
 // @desc Test route 
 // @access Public
@@ -23,17 +26,58 @@ router.post(
             min: 5
         })
     ],
-
-    (req, res) => {
+    async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({
                 errors: errors.array()
             });
         }
+        const {
+            name,
+            email,
+            password
+        } = req.body;
+        try {
+            // verifie si l'user exist 
+            let user = await User.findOne({
+                email
+            });
+            if (user) {
+                res.status(400).json({
+                    errors: [{
+                        msg: 'Utilisateur déja existant'
+                    }]
+                });
+            }
+            const avatar = gravatar.url(email, {
+                s: '200',
+                r: 'pg',
+                d: 'mm'
+            })
+            user = new User({
+                name,
+                email,
+                avatar,
+                password
+            })
 
-        
-        res.send('User route');
+
+            // eencrypt password 
+            const salt = await bcrypt.genSalt(10);
+
+            user.password = await bcrypt.hash(password, salt);
+
+            await user.save();
+
+            res.send('Utilisateur inscrit');
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('server error');
+
+        }
+
+
     }
 );
 
