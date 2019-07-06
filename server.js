6f53aa90f4a4461a4ecf7094330672c06ad6da5c
najlabenchelly <chelly.n103@gmail.com>
2019-07-06 03:22:15 +0200

    création des routees

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
-const express = require("express");
-
+const express = require('express');
+const connectDB = require('./config/db');
 const app = express();
-
-app.get("/", (req, res) => res.send("API Running"));
+// connexion DB 
+connectDB();
+app.get("/", (req, res) => res.send('API Running'));
+//Definit les routee
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
 
 const PORT = process.env.PORT || 5000;
 
