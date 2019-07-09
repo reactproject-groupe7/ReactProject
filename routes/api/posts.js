a9e8c0c173a10d27ff2e8b0119146e4de20d4fc2
najlabenchelly <chelly.n103@gmail.com>
2019-07-09 22:58:41 +0200

    post comment/ add & deleeete

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -120,7 +120,7 @@ router.delete('/:id', auth, async (req, res) => {
         // Check user
         if (post.user.toString() !== req.user.id) {
             return res.status(401).json({
-                msg: 'User n pas les droits'
+                msg: 'User n&#130 a pas les droits'
             });
         }
 
@@ -151,7 +151,7 @@ router.put('/like/:id', auth, async (req, res) => {
             post.likes.filter(like => like.user.toString() === req.user.id).length > 0
         ) {
             return res.status(400).json({
-                msg: 'Vous aimez déja article'
+                msg: 'Vous aimez déjà article'
             });
         }
 
@@ -179,7 +179,7 @@ router.put('/like/:id', auth, async (req, res) => {
                 0
             ) {
                 return res.status(400).json({
-                    msg: 'Article nest pas encore aimé'
+                    msg: 'Article n&#130est pas encore aimé'
                 });
             }
 
@@ -198,5 +198,90 @@ router.put('/like/:id', auth, async (req, res) => {
             res.status(500).send('Server Error');
         }
     });
+});
+
+// @route    POST api/posts/comment/:id
+// @desc     Comment on a post
+// @access   Private
+router.post(
+    '/comment/:id',
+    [
+        auth,
+        [
+            check('text', 'Le texte est obligatoire')
+            .not()
+            .isEmpty()
+        ]
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array()
+            });
+        }
+
+        try {
+            const user = await User.findById(req.user.id).select('-password');
+            const post = await Post.findById(req.params.id);
+
+            const newComment = {
+                text: req.body.text,
+                name: user.name,
+                avatar: user.avatar,
+                user: req.user.id
+            };
+
+            post.comments.unshift(newComment);
+
+            await post.save();
+
+            res.json(post.comments);
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server Error');
+        }
+    }
+);
+// @route    DELETE api/posts/comment/:id/:comment_id
+// @desc     Delete comment
+// @access   Private
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        // extraire le commentaire 
+        const comment = post.comments.find(
+            comment => comment.id === req.params.comment_id
+        );
+
+        // s'assure que le commentaire est existant 
+        if (!comment) {
+            return res.status(404).json({
+                msg: 'Le commentaire nexiste pas '
+            });
+        }
+
+        // verifie l'user 
+        if (comment.user.toString() !== req.user.id) {
+            return res.status(401).json({
+                msg: 'Utilisateur non autorisé'
+            });
+        }
+
+        // Efface l'index
+        const removeIndex = post.comments
+            .map(comment => comment.id)
+            .indexOf(req.params.comment_id);
+
+        post.comments.splice(removeIndex, 1);
+
+        await post.save();
+
+        res.json(post.comments);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
 });
 module.exports = router;
