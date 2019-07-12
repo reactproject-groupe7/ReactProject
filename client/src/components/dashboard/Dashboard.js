b9af670be9d8521ac87e1b1ee8d46650a6427635
najlabenchelly <chelly.n103@gmail.com>
2019-07-12 21:46:42 +0200

    protection des routees pour le dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 
 const Dashboard = () => {
     return (
@@ -6,6 +7,10 @@ const Dashboard = () => {
             Dashboard
         </div>
     )
+}
+Dashboard.propTypes = {
+    
+    
 }
 
 export default Dashboard
